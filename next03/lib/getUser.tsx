03e154ae88bb7e49cdfb68ef7a7c46b733588fa7
Elyse Montano <elysemontano.dev.com>
2024-02-01 19:18:17 -0800

    updates user fetch calls to be ssg

--- next03/lib/getUser.tsx
+++ next03/lib/getUser.tsx
@@ -4,7 +4,7 @@ const getUser = async (userId: string) => {
 
   const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
 
-  if(!res.ok) throw new Error('failed to fetch user')
+  if(!res.ok) undefined
 
   return res.json()
 }
